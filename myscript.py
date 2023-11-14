1daf018114be159bc45f00b6cc7d7f834b2e755a
MathieuGrenier446 <90001228+MathieuGrenier446@users.noreply.github.com>
2023-11-14 16:11:43 -0500

    a

--- myscript.py
+++ myscript.py
@@ -11,9 +11,9 @@ import subprocess
 badhash = "01c7fa9ff06348f539599aad591836c129a3ab13" 
 goodhash = "e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c"
 
-output_1 = subprocess.check_output(f"git bisect start {badhash} {goodhash}", shell=True, check=False)
-output_2 = subprocess.check_output("git bisect run manage.py test", shell=True, check=False)
-output_3 = subprocess.check_output("git bisect reset", shell=True, check=False)
+output_1 = subprocess.run(f"git bisect start {badhash} {goodhash}", shell=True, check=False).stdout
+output_2 = subprocess.run("git bisect run manage.py test", shell=True, check=False).stdout
+output_3 = subprocess.run("git bisect reset", shell=True, check=False).stdout
 
 print(output_1)
 print(output_2)
