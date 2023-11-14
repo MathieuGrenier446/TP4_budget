30d001fabc96fe10070b5fac7f4807bdfcad121e
MathieuGrenier446 <90001228+MathieuGrenier446@users.noreply.github.com>
2023-11-14 15:20:57 -0500

    a

--- myscript.py
+++ myscript.py
@@ -11,14 +11,14 @@ import subprocess
 badhash = "01c7fa9ff06348f539599aad591836c129a3ab13" 
 goodhash = "e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c"
 
-# os.system(f'git bisect start {badhash} {goodhash}')
-# os.system('git bisect run manage.py test')
-# os.system('git bisect reset')
+os.system(f'git bisect start {badhash} {goodhash}')
+os.system('git bisect run manage.py test')
+os.system('git bisect reset')
 
-output_1 = subprocess.check_output(f"git bisect start {badhash} {goodhash}", shell=True)
-output_2 = subprocess.check_output("git bisect run manage.py test", shell=True)
-output_3 = subprocess.check_output("git bisect reset", shell=True)
+# output_1 = subprocess.check_output(f"git bisect start {badhash} {goodhash}", shell=True)
+# output_2 = subprocess.check_output("git bisect run manage.py test", shell=True)
+# output_3 = subprocess.check_output("git bisect reset", shell=True)
 
-print(output_1)
-print(output_2)
-print(output_3)
+# print(output_1)
+# print(output_2)
+# print(output_3)
