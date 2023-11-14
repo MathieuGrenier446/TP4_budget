01c7fa9ff06348f539599aad591836c129a3ab13
MathieuGrenier446 <90001228+MathieuGrenier446@users.noreply.github.com>
2023-11-14 14:52:43 -0500

    arrete

--- myscript.py
+++ myscript.py
@@ -1,5 +1,6 @@
-import os
+# import os
 import argparse
+import subprocess
 
 parser = argparse.ArgumentParser()
 parser.add_argument("badhash", help="The hash of the bad commit")
@@ -8,5 +9,8 @@ parser.add_argument("goodhash", help="The hash of the good commit")
 badhash = parser.parse_args().badhash
 goodhash = parser.parse_args().goodhash
 
-os.system(f'git bisect start {badhash} {goodhash}')
-os.system('git bisect run manage.py test')
+# os.system(f'git bisect start {badhash} {goodhash}')
+# os.system('git bisect run manage.py test')
+
+subprocess.run(['git', 'bisect', 'start', badhash, goodhash], check=True)
+subprocess.run(['git', 'bisect', 'run', 'manage.py', 'test'], check=True)
