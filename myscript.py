329924763aa68796e25d19ad5372ba7d39035375
MathieuGrenier446 <90001228+MathieuGrenier446@users.noreply.github.com>
2023-11-14 14:24:07 -0500

    Feat: workflow + myscript

---
+++ myscript.py
@@ -0,0 +1,12 @@
+import os
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("badhash", help="The hash of the bad commit")
+parser.add_argument("goodhash", help="The hash of the good commit")
+
+badhash = parser.parse_args().badhash
+goodhash = parser.parse_args().goodhash
+
+os.system(f'git bisect start {badhash} {goodhash}')
+os.system('git bisect run manage.py test')
